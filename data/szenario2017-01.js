3cb9dd11dbd46836a46cbe687bf753576a7098f7
Johannes <johannes.mainusch@kommitment.biz>
2018-05-12 21:02:01 +0200

    added a couple of Kommanditists

--- data/szenario2017-01.js
+++ data/szenario2017-01.js
@@ -1,38 +1,57 @@
 var data = [
 	{
-	Abrechenzeitpunkt: "2016-12-31",
-	Contribution: "324149.68",
-	kommanditisten: [
-		{ Name: "Anke N.", Arbeit: "100%" },
-		{ Name: "Ralf W.", Arbeit: "100%" },
-		{ Name: "Johannes M.", Arbeit: "100%" }]
-},
-{
-	Abrechenzeitpunkt: "2017-12-31",
-	Contribution: "547152.82",
-	kommanditisten: [
-		{ Name: "Anke N.", Arbeit: "100%" },
-		{ Name: "Ralf W.", Arbeit: "100%" },
-		{ Name: "Johannes M.", Arbeit: "100%" }]
-},
-{
-	Abrechenzeitpunkt: "2018",
-	Contribution: "600000",
-	kommanditisten: [
-		{ Name: "Anke N.", Arbeit: "100%" },
-		{ Name: "Ralf W.", Arbeit: "0%", returnedFairShares: "100%" },
-		{ Name: "Ben W.", Arbeit: "100%" },
-		{ Name: "Katja R.", Arbeit: "25%" },
-		{ Name: "Johannes M.", Arbeit: "100%" }]
-}
-, {
-	Abrechenzeitpunkt: "2019",
-	Contribution: "600000	",
-	kommanditisten: [
-		{ Name: "Anke N.", Arbeit: "100%" },
-		{ Name: "Ralf W.", Arbeit: "0%" },
-		{ Name: "Ben W.", Arbeit: "100%" },
-		{ Name: "Katja R.", Arbeit: "100%" },
-		{ Name: "Johannes M.", Arbeit: "100%" }]
+		Abrechenzeitpunkt: "2016-12-31",
+		Contribution: "324149.68",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ralf W.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" }
+		]
+	},
+	{
+		Abrechenzeitpunkt: "2017-12-31",
+		Contribution: "547152.82",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ralf W.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" }
+		]
+	},
+	{
+		Abrechenzeitpunkt: "2018",
+		Contribution: "600000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ralf W.", Arbeit: "0%", returnedFairShares: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "25%" },
+			{ Name: "Johannes M.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2019",
+		Contribution: "800000	",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2020",
+		Contribution: "800000	",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
 	}
 ]
