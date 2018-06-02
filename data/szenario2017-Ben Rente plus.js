a5906c66014fade3cfc48bb93fc227e8b4ca707e
Ben <benjamin.wiedenmann@kommitment.biz>
2018-06-02 09:06:01 +0200

    zwei neue Szenarien
    Signed-off-by: Ben <benjamin.wiedenmann@kommitment.biz>

---
+++ data/szenario2017-Ben Rente plus.js
@@ -0,0 +1,558 @@
+var data = [
+	{
+		Abrechenzeitpunkt: "2016-12-31",
+		Contribution: "324149.68",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ralf W.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" }
+		]
+	},
+	{
+		Abrechenzeitpunkt: "2017-12-31",
+		Contribution: "547152.82",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ralf W.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" }
+		]
+	},
+	{
+		Abrechenzeitpunkt: "2018",
+		Contribution: "600000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ralf W.", Arbeit: "0%", returnedFairShares: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "25%" },
+			{ Name: "Johannes M.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2019",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2020",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2021",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2022",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2023",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2025",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2026",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2027",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2028",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2029",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2030",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2031",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2032",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2033",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2034",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2035",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2036",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2037",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2038",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2039",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2040",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2041",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2042",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2043",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2044",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2045",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2046",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2047",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2048",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "0%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" },
+			{ Name: "mitmacher 3", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2049",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "0%" },
+			{ Name: "Ben W.", Arbeit: "0%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" },
+			{ Name: "mitmacher 3", Arbeit: "100%" },
+			{ Name: "mitmacher 4", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2050",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "0%" },
+			{ Name: "Ben W.", Arbeit: "0%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" },
+			{ Name: "mitmacher 3", Arbeit: "100%" },
+			{ Name: "mitmacher 4", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2051",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "0%" },
+			{ Name: "Ben W.", Arbeit: "0%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" },
+			{ Name: "mitmacher 3", Arbeit: "100%" },
+			{ Name: "mitmacher 4", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2052",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "0%" },
+			{ Name: "Ben W.", Arbeit: "0%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" },
+			{ Name: "mitmacher 3", Arbeit: "100%" },
+			{ Name: "mitmacher 4", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2053",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "0%" },
+			{ Name: "Ben W.", Arbeit: "0%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" },
+			{ Name: "mitmacher 3", Arbeit: "100%" },
+			{ Name: "mitmacher 4", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2054",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "0%" },
+			{ Name: "Ben W.", Arbeit: "0%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" },
+			{ Name: "mitmacher 3", Arbeit: "100%" },
+			{ Name: "mitmacher 4", Arbeit: "100%" }
+		]
+	}
+	, {
+		Abrechenzeitpunkt: "2055",
+		Contribution: "800000",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "0%" },
+			{ Name: "Ben W.", Arbeit: "0%" },
+			{ Name: "Katja R.", Arbeit: "0%" },
+			{ Name: "Johannes M.", Arbeit: "0%" },
+			{ Name: "Ingo K.", Arbeit: "100%" },
+			{ Name: "Joris S.", Arbeit: "100%" },
+			{ Name: "Marcel B.", Arbeit: "100%" },
+			{ Name: "mitmacher 1", Arbeit: "100%" },
+			{ Name: "mitmacher 2", Arbeit: "100%" },
+			{ Name: "mitmacher 3", Arbeit: "100%" },
+			{ Name: "mitmacher 4", Arbeit: "100%" }
+		]
+	}
+]
