819780f557316567572922658d6a2367e5d38d2c
Ben <benjamin.wiedenmann@kommitment.biz>
2019-03-06 22:58:18 +0100

    szenario05.js bereinigt
    - conrtibution entfernt
    - szenario leicht auf langsames wachstum angepasst

--- data/szenario05.js
+++ data/szenario05.js
@@ -1,50 +1,54 @@
 var data = [
 		{
 		Abrechenzeitpunkt: "2016-12-31",
-		Contribution: "97000",
-		kommanditisten: [
+			kommanditisten: [
 			{ Name: "Anke N.", Arbeit: "100%" },
 			{ Name: "Ralf W.", Arbeit: "100%" },
-			{ Name: "Johannes M.", Arbeit: "100%" }]
+			{ Name: "Johannes M.", Arbeit: "100%"}]
 	},
 	{
 		Abrechenzeitpunkt: "2017-12-31",
-		Contribution: "120000",
 		kommanditisten: [
 			{ Name: "Anke N.", Arbeit: "100%" },
 			{ Name: "Ralf W.", Arbeit: "100%" },
-			{ Name: "Johannes M.", Arbeit: "100%" }]
+			{ Name: "Johannes M.", Arbeit: "100%"}]
 	},
 	{
 		Abrechenzeitpunkt: "2018",
-		Contribution: "140000",
 		kommanditisten: [
 			{ Name: "Anke N.", Arbeit: "100%" },
-			{ Name: "Ralf W.", Arbeit: "100%" },
+			{ Name: "Ralf W.", Arbeit: "0%", returnedFairShares: "100%" },
 			{ Name: "Ben W.", Arbeit: "100%" },
-			{ Name: "Johannes M.", Arbeit: "100%" }]
+			{ Name: "Katja R.", Arbeit: "25%" },
+			{ Name: "Johannes M.", Arbeit: "100%"}]
 	}
 	, {
 		Abrechenzeitpunkt: "2019",
-		Contribution: "170000	",
 		kommanditisten: [
 			{ Name: "Anke N.", Arbeit: "100%" },
-			{ Name: "Ralf W.", Arbeit: "100%" },
 			{ Name: "Ben W.", Arbeit: "100%" },
 			{ Name: "Katja R.", Arbeit: "100%" },
-			{ Name: "Johannes M.", Arbeit: "100%" }]
+			{ Name: "Johannes M.", Arbeit: "100%"},
+			{ Name: "Egon01", Arbeit: "75%" }]
 		}
 	, {
 		Abrechenzeitpunkt: "2020",
-		Contribution: "200000",
 		kommanditisten: [
 			{ Name: "Anke N.", Arbeit: "100%" },
-			{ Name: "Ralf W.", Arbeit: "100%" },
 			{ Name: "Ben W.", Arbeit: "100%" },
 			{ Name: "Katja R.", Arbeit: "100%" },
-			{ Name: "Johannes M.", Arbeit: "100%" },
-			{ Name: "Egon01", Arbeit: "100%" },
-			{ Name: "Egon02", Arbeit: "100%" },
-			{ Name: "Hulk.", Arbeit: "100%" }]
+			{ Name: "Johannes M.", Arbeit: "100%"},
+			{ Name: "Egon01", Arbeit: "100%" }]
 		}
+		, {
+			Abrechenzeitpunkt: "2021",
+			kommanditisten: [
+				{ Name: "Anke N.", Arbeit: "100%" },
+				{ Name: "Ben W.", Arbeit: "100%" },
+				{ Name: "Katja R.", Arbeit: "100%" },
+				{ Name: "Johannes M.", Arbeit: "100%"},
+				{ Name: "Egon01", Arbeit: "100%" },
+				{ Name: "Egon02", Arbeit: "100%" },
+				{ Name: "Hulk.", Arbeit: "100%" }]
+			}
 ]
