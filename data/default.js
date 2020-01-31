90cfdd7b68bba2dd41c508110d796b8e821499f2
Johannes <johannes.mainusch@kommitment.biz>
2020-01-31 12:48:32 +0100

    added new year

--- data/default.js
+++ data/default.js
@@ -19,15 +19,30 @@ var data = [
 		{ Name: "Anke N.", Arbeit: "100%" },
 		{ Name: "Ralf W.", Arbeit: "0%", returnedFairShares: "100%" },
 		{ Name: "Ben W.", Arbeit: "100%" },
-		{ Name: "Katja R.", Arbeit: "25%" },
 		{ Name: "Johannes M.", Arbeit: "100%" }]
-}
-, {
+},
+{
 	Abrechenzeitpunkt: "2019",
 	kommanditisten: [
 		{ Name: "Anke N.", Arbeit: "100%" },
 		{ Name: "Ben W.", Arbeit: "100%" },
-		{ Name: "Katja R.", Arbeit: "100%" },
 		{ Name: "Johannes M.", Arbeit: "100%" }]
+	},
+	{
+		Abrechenzeitpunkt: "2020",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" }]
+	},
+	{
+		Abrechenzeitpunkt: "2021",
+		kommanditisten: [
+			{ Name: "Anke N.", Arbeit: "100%" },
+			{ Name: "N1. N.", Arbeit: "100%" },
+			{ Name: "N2. N.", Arbeit: "100%" },
+			{ Name: "Sven R.", Arbeit: "100%" },
+			{ Name: "Ben W.", Arbeit: "100%" },
+			{ Name: "Johannes M.", Arbeit: "100%" }]
 	}
 ]
