3c10a6691997b2fe607b6d362d4d5bd99ce1cbed
Jost Künzel <jost.kuenzel@zinsland.de>
2020-05-15 19:12:40 +0200

    feat: basic layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,23 @@
 <template lang="pug">
-  div.container
-    h1 FairShare
-      br
-      small FairShare is a modell of fair share distributions in companies.
-    nuxt
+  b-container
+    b-navbar(toggleable="lg" type="dark" variant="info")
+      b-navbar-brand FairShare
+
+      b-navbar-toggle(target="nav-collapse")
+
+      b-collapse(id="nav-collapse" is-nav)
+        b-navbar-nav
+          b-nav-item(to="/" :active="$route.name=='index'") Home
+          b-nav-item(to="demo" :active="$route.name=='demo'") Demo
+
+        b-navbar-nav.ml-auto
+          b-nav-item(href="https://github.com/kommitment/fairshare") GitHub
+
+    b-container.mt-4
+      .row
+        .col
+          h3.my-2 FairShare - einfach "fairteilen"
+      .row
+        .col
+          nuxt
 </template>
--- src/pages/demo.vue
+++ src/pages/demo.vue
@@ -1,4 +1,48 @@
 <template lang="pug">
   div
-    h3 Demo
+    .row
+      .col
+        p.lead
+          | Input parameters of FairShare is a set of revenue related data per persiod (e.g. per year).
+          br
+          | FairShare will calculate the
+          |           share of every partner of the company and show the change of his/her share per period. So the plan.
+
+        form.my-4
+          .row
+            .col.col-sm-6.col-md-3
+              .form-group
+                label Founders keep shares:&nbsp;
+                  span {{foundersShares}}%
+                input.slider(type="range" min="0" max="50" step="0.1" v-model="foundersShares")
+    .row
+      .col
+        .p-3
+          #chartId
+    .row
+      .col
+        .p-3
+          #filterDiv
+    .row
+      .col
+        .p-3
+          #page-wrap
+    .row
+      .col
+        pre {{dataset}}
+
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'nuxt-class-component'
+import dataset from '@/datasets/default'
+
+@Component({})
+export default class Demo extends Vue {
+  foundersShares: number = 6.5
+  get dataset() {
+    return dataset
+  }
+}
+</script>
