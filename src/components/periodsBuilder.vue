fca01f77074e07d0dae61c8eaaf7d046a2bf03e9
Jost Künzel <jost.kuenzel@zinsland.de>
2020-06-19 22:20:43 +0200

    feat: sorting of partners, add founders

--- src/components/periodsBuilder.vue
+++ src/components/periodsBuilder.vue
@@ -3,17 +3,20 @@
     b-form
       p Persons
       b-card-group.mb-4
-        template(v-for="(p, idx) in partnerNames.sort()" deck)
-          b-card(:title="p" :sub-title="isFounder(p) ? '⭐ founder' : 'partner'")
+        template(v-for="(p, idx) in sort(partnerNames)" deck)
+          b-card(:title="p")
+            b-card-text
+              b-link(@click="onClickType(p)") {{isFounder(p) ? 'founder' : 'partner'}}
 
       p Periods
       template(v-for="(period, idx) in periods")
         b-card.mb-2(:title="period.date")
           b-card-group.mb-4
-            template(v-for="(p, idx) in getPartnersSortedByName(period.partners)")
-              b-card(:title="p.name" :sub-title="isFounder(p.name) ? '⭐ founder' : 'partner'")
+            template(v-for="(p, idx) in sortByPartnerName(period.partners)")
+              b-card(:title="p.name" :sub-title="isFounder(p.name) ? 'founder' : 'partner'")
                 b-card-text
-                  div Contribution {{p.work * 100}}%
+                  div() Contribution {{p.work * 100}}%
+                    b-form-input(type="range" min="0" max="1" step="0.05")
                   div(v-if="p.returnedFairShares") Returned FairShares {{p.returnedFairShares * 100}}%
 
       b-button.mt-4(@click="onClick") Emit Test Data
@@ -22,12 +25,28 @@
 <script lang="ts">
 import Vue from 'vue'
 import Component from 'nuxt-class-component'
-import { includes, sortBy, prop } from 'ramda'
+import { Watch } from 'vue-property-decorator'
+import {
+  includes,
+  prop,
+  without,
+  ascend,
+  sort,
+  sortBy,
+  identity,
+  concat,
+  map,
+} from 'ramda'
 import dataset from '@/datasets/default'
 import extractPartnerNames from '@/lib/extractPartnerNames'
 import extractFounderNames from '@/lib/extractFounderNames'
 
-@Component({})
+@Component({
+  methods: {
+    sort: sort(ascend(identity)),
+    sortByPartnerName: sortBy(prop('name')),
+  },
+})
 export default class PeriodsBuilder extends Vue {
   periods: Period[] = []
   partnerNames: string[] = []
@@ -51,16 +70,25 @@ export default class PeriodsBuilder extends Vue {
     return this.periods
   }
 
-  get founders(): Partner[] {
-    return this.periods.length ? this.periods[0].partners : []
-  }
-
   isFounder(partner: string): boolean {
     return includes(partner, this.founderNames)
   }
 
-  getPartnersSortedByName(partners: Partner[]): Partner[] {
-    return sortBy(prop('name'), partners)
+  onClickType(partnerName: string) {
+    this.founderNames = this.isFounder(partnerName)
+      ? without([partnerName], this.founderNames)
+      : concat([partnerName], this.founderNames)
+  }
+
+  @Watch('founderNames')
+  watchFounderNames() {
+    // add founder to first period
+    if (!this.periods.length) return
+    const partners = map(
+      (name: string) => ({ name, work: 1 }),
+      this.founderNames
+    )
+    this.periods[0].partners = partners
   }
 }
 </script>
