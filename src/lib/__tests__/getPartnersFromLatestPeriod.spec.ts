8fd233d311a0b4ccb42956bde756850c8bc2ef26
Jost Künzel <jost.kuenzel@zinsland.de>
2020-07-06 12:33:49 +0200

    feat: getPartnersFromLatestPeriod excludes leavnig partners

--- src/lib/__tests__/getPartnersFromLatestPeriod.spec.ts
+++ src/lib/__tests__/getPartnersFromLatestPeriod.spec.ts
@@ -6,11 +6,24 @@ describe('getPartnersFromLatestPeriod', () => {
   })
 
   test('Returns partners', () => {
-    const partners = [
+    const partners: Partner[] = [
       { name: 'Hugo', work: 1 },
       { name: 'Kati', work: 1 },
     ]
     const periods = [{ name: 'Year 1', partners }]
     expect(getPartnersFromLatestPeriod(periods)).toStrictEqual(partners)
+  })
+
+  test('Returns partners without partners that left', () => {
+    const expectedPartners: Partner[] = [
+      { name: 'Hugo', work: 1 },
+      { name: 'Kati', work: 1 },
+    ]
+    const excludedPartners: Partner[] = [
+      { name: 'Murat', work: 1, returnedFairShares: 1 },
+    ]
+    const partners: Partner[] = [...excludedPartners, ...expectedPartners]
+    const periods = [{ name: 'Year 1', partners }]
+    expect(getPartnersFromLatestPeriod(periods)).toStrictEqual(expectedPartners)
   })
 })
--- src/lib/getPartnersFromLatestPeriod.ts
+++ src/lib/getPartnersFromLatestPeriod.ts
@@ -1,8 +1,19 @@
-import { pipe, ifElse, last, always, clone } from 'ramda'
+import { pipe, ifElse, last, always, clone, reject } from 'ramda'
 
 export default (periods: Period[]): Partner[] =>
   ifElse(
     (periods) => periods.length > 0,
-    pipe(last, (p: Period) => clone(p.partners)),
+    getPartnersFromLatestPeriod,
     always([])
   )(periods)
+
+const getPartnersFromLatestPeriod = (periods: Period[]): Partner[] =>
+  pipe(
+    //
+    last,
+    (p: Period) => clone(p.partners),
+    reject(hasReturnedFairShares)
+  )(periods)
+
+const hasReturnedFairShares = (p: Partner): boolean =>
+  (p.returnedFairShares && p.returnedFairShares > 0) as boolean
