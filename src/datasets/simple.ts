c541c15d0e990489cf29e0fdc8994a69b8b2a28d
Jost Künzel <jost.kuenzel@zinsland.de>
2020-07-06 11:21:57 +0200

    feat: add simple dataset

---
+++ src/datasets/simple.ts
@@ -0,0 +1,25 @@
+const history: Period[] = [
+  {
+    name: 'Year 1',
+    partners: [
+      { name: 'Ernie', work: 1 },
+      { name: 'Bert', work: 1 },
+    ],
+  },
+  {
+    name: 'Year 2',
+    partners: [
+      { name: 'Bert', work: 1 },
+      { name: 'Ernie', work: 1 },
+    ],
+  },
+  {
+    name: 'Year 3',
+    partners: [
+      { name: 'Ernie', work: 1 },
+      { name: 'Bert', work: 1 },
+    ],
+  },
+]
+
+export default history
