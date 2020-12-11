92e500f117c2d4bd4da6a8b937dbd8065e3f2f7f
Jost Künzel <jost.kuenzel@zinsland.de>
2020-12-11 18:20:07 +0100

    feat: add formatting to percent

--- src/components/BarChart.vue
+++ src/components/BarChart.vue
@@ -3,6 +3,10 @@ import { Bar, mixins } from 'vue-chartjs'
 import { Component, Mixins } from 'vue-property-decorator'
 
 const { reactiveProp } = mixins
+const toPercent = (val: any, precision: number = 4): string =>
+  new Intl.NumberFormat('de-DE', {
+    maximumSignificantDigits: precision,
+  }).format(val * 100)
 
 @Component
 export default class BarChart extends Mixins(Bar, reactiveProp) {
@@ -12,7 +16,26 @@ export default class BarChart extends Mixins(Bar, reactiveProp) {
       text: 'Verteilung der FairShares unter den Mitmachern',
     },
     responsive: true,
-    scales: { xAxes: [{ stacked: true }], yAxes: [{ stacked: true }] },
+    tooltips: {
+      callbacks: {
+        label(tooltipItem: any, data: any) {
+          return `${data.datasets[tooltipItem.datasetIndex].label}: ${toPercent(
+            tooltipItem.yLabel
+          )}%`
+        },
+      },
+    },
+    scales: {
+      xAxes: [{ stacked: true }],
+      yAxes: [
+        {
+          stacked: true,
+          ticks: {
+            callback: (val: any) => `${toPercent(val)}%`,
+          },
+        },
+      ],
+    },
   }
 
   mounted() {
